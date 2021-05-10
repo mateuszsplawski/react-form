a8b44c3b679cdbadc045a79b93d827dbe888a9fb
Mateusz <mateusz.splawski96@gmail.com>
2021-05-10 20:03:19 +0200

    style(views, index): Style main view

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.css";
+import "index.css";
 
 import { Main } from "./views/Main/Main";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Main />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,11 +1,22 @@
+import { Card, Col, Row } from "antd";
 import Layout from "antd/lib/layout/layout";
 
 import { ContractorForm } from "components/ContractorForm/ContractorForm";
 
 export const Main = () => {
+  const layoutStyle = {
+    minHeight: "100%",
+    justifyContent: "center",
+  };
   return (
-    <Layout>
-      <ContractorForm />
+    <Layout style={layoutStyle}>
+      <Row align="middle" justify="center">
+        <Col xl={10} lg={14} md={18} sm={20} xs={22}>
+          <Card bordered>
+            <ContractorForm />
+          </Card>
+        </Col>
+      </Row>
     </Layout>
   );
 };
