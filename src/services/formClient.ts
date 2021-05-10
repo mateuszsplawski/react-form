278e3d1682e9a83570ebe5bb7535eefad4ed6107
Mateusz <mateusz.splawski96@gmail.com>
2021-05-10 20:01:22 +0200

    feat(services): Implement ky client with add contractor func

--- src/services/formClient.ts
+++ src/services/formClient.ts
@@ -1,11 +1,13 @@
-import ky from "ky";
+import ky from "ky/distribution";
+
+import { ContractorData } from "types/index";
 
 const prefixUrl = "https://localhost:60001/";
 const client = ky.create({ prefixUrl });
 
 const formClient = {
-  addContractor({ contractorData }: { contractorData: any }) {
-    client.post("Contractor/Save", { json: { contractorData } });
+  addContractor({ contractorData }: { contractorData: ContractorData }) {
+    return client.post("Contractor/Save", { json: { contractorData } });
   },
 };
 
